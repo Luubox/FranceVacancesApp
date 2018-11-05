68222be00d9ccafd49dbd182fc407cb058aa7d67
Fr0thgar <jona270r@edu.easj.dk>
2018-11-05 11:08:51 +0100

    Region Constructor

--- FranceVacancesApp/Model/Region.cs
+++ FranceVacancesApp/Model/Region.cs
@@ -8,5 +8,16 @@ namespace FranceVacancesApp.Model
 {
     class Region
     {
+        private string _seværdigheder;
+        private string _valuta;
+        private string _info;
+
+        public Region(string seværdigheder, string valuta, string info)
+        {
+            _seværdigheder = seværdigheder;
+            _valuta = valuta;
+            _info = info;
+        }
     }
+
 }
