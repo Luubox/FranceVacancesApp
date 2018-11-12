0ab27fdca6ee572146e20d5348fcde1f0b5f59f8
J Nielsen <buhros@hotmail.com>
2018-11-12 13:24:10 +0100

    Viewmodel

--- FranceVacanceUWP/View/MainPage.xaml.cs
+++ FranceVacanceUWP/View/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Windows.UI.Xaml.Controls;
+using FranceVacanceUWP.Model;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -12,6 +13,8 @@ namespace FranceVacanceUWP.View
         public MainPage()
         {
             this.InitializeComponent();
+            
+
         }
     }
 }
---
+++ FranceVacanceUWP/ViewModel/BoligViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using FranceVacanceUWP.Annotations;
+using FranceVacanceUWP.Model;
+
+namespace FranceVacanceUWP.ViewModel
+{
+    class BoligViewModel : INotifyPropertyChanged
+    {
+        //public BoligCatalog BoligCatalog { get; set; }
+
+        //TODO: binding halløjsa
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(
+            [CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this,
+                new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
